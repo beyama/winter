c4804032ea3411c990f4378d68c5a9ab09674179
Alexander Jentz <alexander@jentz.io>
2018-11-07 17:11:13 +0100

    Add WinterApplication documentation and test

--- winter/src/main/kotlin/io/jentz/winter/WinterApplication.kt
+++ winter/src/main/kotlin/io/jentz/winter/WinterApplication.kt
@@ -2,24 +2,68 @@ package io.jentz.winter
 
 import io.jentz.winter.plugin.PluginRegistry
 
+/**
+ * [WinterApplication] base class that holds Winter plugins and may be configured with the
+ * application [Component].
+ *
+ * @see Winter
+ */
 open class WinterApplication() {
 
+    /**
+     * Convenient constructor to configure the application [Component] during initialization.
+     *
+     * Example:
+     * ```
+     * object MyLibApp : WinterApplication {
+     *   // ... declaration of dependencies
+     * }
+     * ```
+     *
+     * @param qualifier An optional qualifier for the component.
+     * @param block The component builder block.
+     */
     constructor(qualifier: Any? = null, block: ComponentBuilderBlock) : this() {
         component(qualifier, block)
     }
 
+    /**
+     * The [PluginRegistry] of this application.
+     */
     val plugins = PluginRegistry()
 
-    private var _component: Component? = null
+    /**
+     * The application component.
+     */
+    var component = emptyComponent()
 
-    val component: Component
-        get() = _component ?: throw WinterException("component is not configured")
-
+    /**
+     * Sets the application component by supplying an optional qualifier and a component builder
+     * block.
+     *
+     * @param qualifier The optional qualifier for the new component.
+     * @param block The component builder block.
+     */
     fun component(qualifier: Any? = null, block: ComponentBuilderBlock) {
-        _component = io.jentz.winter.component(qualifier, block)
+        this.component = io.jentz.winter.component(qualifier, block)
     }
 
+    /**
+     * Derive the application component and replace it with the derived version.
+     *
+     * @param qualifier The optional qualifier for the derived component.
+     * @param block The component builder block.
+     */
+    fun derive(qualifier: Any? = null, block: ComponentBuilderBlock) {
+        component = component.derive(qualifier, block)
+    }
+
+    /**
+     * Initialize and return the dependency graph from the configured [component].
+     *
+     * @param block An optional component builder block to add additional dependencies.
+     * @return The new [Graph].
+     */
     fun init(block: ComponentBuilderBlock? = null): Graph = component.init(this, block)
 
 }
-
---
+++ winter/src/test/kotlin/io/jentz/winter/WinterApplicationTest.kt
@@ -0,0 +1,56 @@
+package io.jentz.winter
+
+import io.kotlintest.matchers.boolean.shouldBeTrue
+import io.kotlintest.matchers.types.shouldBeSameInstanceAs
+import io.kotlintest.shouldBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class WinterApplicationTest {
+
+    private lateinit var app: WinterApplication
+
+    @BeforeEach
+    fun beforeEach() {
+        app = WinterApplication()
+    }
+
+    @Test
+    fun `component should return empty component by default`() {
+        app.component.isEmpty().shouldBeTrue()
+    }
+
+    @Test
+    fun `plugins should be empty by default`() {
+        app.plugins.isEmpty().shouldBeTrue()
+    }
+
+    @Test
+    fun `#component should configure new component`() {
+        app.component("test") { constant("") }
+        app.component.qualifier.shouldBe("test")
+        app.component.size.shouldBe(1)
+    }
+
+    @Test
+    fun `#derive should derive existing component`() {
+        app.component { constant("") }
+        app.derive("test") { constant(1) }
+        app.component.qualifier.shouldBe("test")
+        app.component.size.shouldBe(2)
+    }
+
+    @Test
+    fun `#init should create graph from component`() {
+        val graph = app.init()
+        graph.application.shouldBeSameInstanceAs(app)
+        graph.component.shouldBeSameInstanceAs(app.component)
+    }
+
+    @Test
+    fun `#init with builder block should derive component`() {
+        val graph = app.init { constant("") }
+        graph.component.size.shouldBe(1)
+    }
+
+}
