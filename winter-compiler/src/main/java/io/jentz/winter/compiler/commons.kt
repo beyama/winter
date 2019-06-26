ba84b5c81a6b03f92a1e0d3b29189e77815b3285
Alexander Jentz <alexander@jentz.io>
2019-06-26 07:49:06 +0200

    Move generateGetInstanceCode into kotlinbuilder package

--- winter-compiler/src/main/java/io/jentz/winter/compiler/commons.kt
+++ winter-compiler/src/main/java/io/jentz/winter/compiler/commons.kt
@@ -25,7 +25,7 @@ val PROVIDER_INTERFACE_NAME = ClassName("javax.inject", "Provider")
 val LAZY_INTERFACE_NAME = ClassName("kotlin", "Lazy")
 val GENERATED_ANNOTATION_NAME = ClassName("javax.annotation", "Generated")
 
-val JAVAX_SINGLETON_ANNOTATION_NAME = "javax.inject.Singleton"
+const val JAVAX_SINGLETON_ANNOTATION_NAME = "javax.inject.Singleton"
 
 val ISO8601_FORMAT =
         SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'", Locale.getDefault()).apply {
@@ -65,76 +65,3 @@ val notNullAnnotations = setOf(
 )
 
 fun mapTypeName(typeName: TypeName) = mappedTypes.getOrDefault(typeName, typeName)
-
-private fun simpleName(typeName: TypeName): KotlinCode = buildKotlinCode {
-    when (typeName) {
-        is ClassName -> {
-            import(typeName)
-            append(typeName.simpleName)
-        }
-        is ParameterizedTypeName -> {
-            import(typeName.rawType)
-            append(typeName.rawType.simpleName)
-
-            val arguments = typeName.typeArguments
-                    .map { simpleName(it) }
-                    .onEach { import(it.imports) }
-                    .joinToString(prefix = "<", postfix = ">") { it.code }
-            append(arguments)
-        }
-    }
-}
-
-private fun getInstanceCode(
-        target: String,
-        typeName: TypeName,
-        isNullable: Boolean,
-        qualifier: String?
-): KotlinCode = buildKotlinCode {
-    val args = mutableListOf<String>()
-    if (qualifier != null) args += "\"$qualifier\""
-    if (typeName is ParameterizedTypeName) args += "generics = true"
-
-    val simpleName = simpleName(typeName)
-
-    if (isNullable) {
-        append("${target}instanceOrNull")
-    } else {
-        append("${target}instance")
-    }
-    append("<")
-    appendCode(simpleName)
-    append(">")
-    append("(${args.joinToString()})")
-}
-
-fun generateGetInstanceCode(target: String, e: VariableElement): KotlinCode = buildKotlinCode {
-    val namedAnnotation = e.getAnnotation(Named::class.java)
-            ?: e.enclosingElement.getAnnotation(Named::class.java)
-    val qualifier = namedAnnotation?.value
-    val isNullable = e.isNullable
-
-    when {
-        e.isProvider -> {
-            val genericTypeMirror = (e.asType() as DeclaredType).typeArguments.first()
-            val typeName = mapTypeName(genericTypeMirror.asTypeName())
-
-            import(PROVIDER_INTERFACE_NAME)
-
-            append("Provider { ")
-            appendCode(getInstanceCode(target, typeName, isNullable, qualifier))
-            append(" }")
-        }
-        e.isLazy -> {
-            val genericTypeMirror = (e.asType() as DeclaredType).typeArguments.first()
-            val typeName = mapTypeName(genericTypeMirror.asTypeName())
-            append("lazy { ")
-            appendCode(getInstanceCode(target, typeName, isNullable, qualifier))
-            append(" }")
-        }
-        else -> {
-            val typeName = mapTypeName(e.asType().asTypeName())
-            appendCode(getInstanceCode(target, typeName, isNullable, qualifier))
-        }
-    }
-}
