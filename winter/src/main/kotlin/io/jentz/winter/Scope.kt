5c5af21a3364bc80943ffe377b32a2e2cb32738c
Alexander Jentz <alexander@jentz.io>
2024-04-14 23:27:10 +0200

    Make scope a value class

--- winter/src/main/kotlin/io/jentz/winter/Scope.kt
+++ winter/src/main/kotlin/io/jentz/winter/Scope.kt
@@ -1,6 +1,11 @@
 package io.jentz.winter
 
-data class Scope(val name: String) {
+@JvmInline
+value class Scope(val name: String) {
+    init {
+        require(name.isNotBlank()) { "Scope name must not be blank." }
+    }
+
     companion object {
         val Prototype = Scope("prototype")
         val Singleton = Scope("singleton")
